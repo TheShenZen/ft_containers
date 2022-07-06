70bc951f74eb346c77f349c543d2e799cec2815c
seciurte <seciurte@student.42.fr>
2022-07-06 18:49:03 +0200

    iterator_traits done and works clean

--- iterator_traits.hpp
+++ iterator_traits.hpp
@@ -2,26 +2,29 @@
 # define FT_ITERATOR_TRAITS_HPP
 
 #include <cstddef>
+#include <iterator>
 
 namespace ft
 {
-	struct	input_iterator_tag {};
-	// marks input iterators -> can only read values and can only be incremented or derefferenced
+	// Doesn't work when used inside the templated struct (still looking why it doesn't)
 
-	struct	output_iterator_tag {};
-	// marks output iterators -> can only change values and can only be incremented or derefferenced
+	// struct	input_iterator_tag {};
+	// // marks input iterators -> can only read values and can only be incremented or derefferenced
 
-	struct	forward_iterator_tag : public input_iterator_tag {};
-	// marks forward iterators -> can read and write and can only be incremented or derefferenced
+	// struct	output_iterator_tag {};
+	// // marks output iterators -> can only change values and can only be incremented or derefferenced
 
-	struct	bidirectional_iterator_tag : public forward_iterator_tag {};
-	// marks the bidirectional iterators -> can read and write, can be incremented or decremented and derferenced
+	// struct	forward_iterator_tag : public input_iterator_tag {};
+	// // marks forward iterators -> can read and write and can only be incremented or derefferenced
 
-	struct	random_acces_iterator_tag : public bidirectional_iterator_tag {};
-	// marks the random access iterators -> can read and write, can be incremented or decremented, dereferenced and offset dereferenced operator
+	// struct	bidirectional_iterator_tag : public forward_iterator_tag {};
+	// // marks the bidirectional iterators -> can read and write, can be incremented or decremented and derferenced
 
-	// std::iterator_traits is the type trait class that provides uniform interface to the properties of LegacyIterator types. This makes it possible to implement algorithms only in terms of iterators.
-	// The template can be specialized for user-defined iterators so that the information about the iterator can be retrieved even if the type does not provide the usual typedefs.
+	// struct	random_access_iterator_tag : public bidirectional_iterator_tag {};
+	// // marks the random access iterators -> can read and write, can be incremented or decremented, dereferenced and offset dereferenced operator
+
+	// // std::iterator_traits is the type trait class that provides uniform interface to the properties of LegacyIterator types. This makes it possible to implement algorithms only in terms of iterators.
+	// // The template can be specialized for user-defined iterators so that the information about the iterator can be retrieved even if the type does not provide the usual typedefs.
 
 	template<class Iterator>
 		struct iterator_traits
@@ -36,7 +39,7 @@ namespace ft
 	template<typename T>
 		struct iterator_traits<T*>
 		{
-			typedef	random_acces_iterator_tag	iterator_category;
+			typedef	std::random_access_iterator_tag	iterator_category;
 			typedef	T							value_type;
 			typedef	std::ptrdiff_t				difference_type;
 			typedef	T*							pointer;
@@ -46,7 +49,7 @@ namespace ft
 	template<typename T>
 		struct iterator_traits<const T*>
 		{
-			typedef	random_acces_iterator_tag	iterator_category;
+			typedef	std::random_access_iterator_tag	iterator_category;
 			typedef	T							value_type;
 			typedef	std::ptrdiff_t				difference_type;
 			typedef	T*							pointer;
